77f0c7f29ec3abf781ebb7cd5770a45cecd36d63
twój login do GitHuba <twj email>
2023-04-04 19:24:11 +0200

    create and implement AppRoutes file

---
+++ src/AppRoutes.tsx
@@ -0,0 +1,64 @@
+import { useRoutes } from "react-router-dom";
+import { DashboardThreeColumns } from "./components/ThreeColumns/DashboardThreeColumns";
+import { DashboardFourSteps } from "./components/FourSteps/DashboardFourSteps";
+import { DashboardAboutUs } from "./components/AboutUs/DashboardAboutUs";
+import { DashboardWhatWeHelp } from "./components/WhatWeHelp /DashboardWhatWeHelp";
+import { DashboardContactForm } from "./components/Contact/DashboardContactForm";
+import { DashboardFooter } from "./components/Footer /DashboardFooter";
+import { DashboardStart } from "./components/Start/DashboardStart";
+import { Dashboard } from "./views/Dashboard/Dashboard";
+import { Signin } from "./views/Signin/Signin";
+import { Signup } from "./views/Signup/Signup";
+import { Logout } from "./views/Logout/Logout";
+
+export const AppRoutes = () => {
+  const routes = useRoutes([
+    {
+      path: "/",
+      element: <Dashboard />,
+      children: [
+        {
+          path: "start",
+          element: <DashboardStart />,
+        },
+        {
+          path: "threeColumns",
+          element: <DashboardThreeColumns />,
+        },
+        {
+          path: "fourSteps",
+          element: <DashboardFourSteps />,
+        },
+        {
+          path: "aboutUs",
+          element: <DashboardAboutUs />,
+        },
+        {
+          path: "weHelp",
+          element: <DashboardWhatWeHelp />,
+        },
+        {
+          path: "contact",
+          element: <DashboardContactForm />,
+        },
+        {
+          path: "footer",
+          element: <DashboardFooter />,
+        },
+      ],
+    },
+    {
+      path: "/signup",
+      element: <Signup />,
+    },
+    {
+      path: "/signin",
+      element: <Signin />,
+    },
+    {
+      path: "/logout",
+      element: <Logout />,
+    },
+  ]);
+  return routes;
+};
