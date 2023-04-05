585bb7bc5a9ea83d63f9e08d0b283fdb8282c861
twój login do GitHuba <twj email>
2023-04-05 11:46:04 +0200

    create and style section four steps

---
+++ src/components/FourSteps/DashboardFourSteps.tsx
@@ -0,0 +1,53 @@
+import { useNavigate } from "react-router-dom";
+import decoration from "../../assets/Decoration.png";
+import pullover from "../../assets/Icon.png";
+import arrow from "../../assets/Icon-arrow.png";
+import glass from "../../assets/Icon-glass.png";
+import loop from "../../assets/Icon-loop.png";
+
+export const DashboardFourSteps = () => {
+  const navigate = useNavigate();
+
+  const handleClickToDonateThing = () => {
+    navigate("/donateThings");
+  };
+  return (
+    <section className="container__four--steps">
+      <div className="header__text--section">
+        <h1 className="title__four--steps">Just 4 simple steps</h1>
+        <img className="decoration" src={decoration} alt="decoration" />
+      </div>
+      <div className="instructions__container--section">
+        <div className="instructions__container">
+          <img className="img" src={pullover} alt="pulloverImg" />
+          <p className="instruction">Choose things</p>
+          <p className="things__to--give">clothes, toys, equipment and more</p>
+        </div>
+        <div className="instructions__container">
+          <img className="img" src={arrow} alt="arrowImg" />
+          <p className="instruction">Choose things</p>
+          <p className="things__to--give">clothes, toys, equipment and more</p>
+        </div>
+        <div className="instructions__container">
+          <img className="img" src={glass} alt="magnifyingGlass" />
+          <p className="instruction">Choose things</p>
+          <p className="things__to--give">clothes, toys, equipment and more</p>
+        </div>
+        <div className="instructions__container">
+          <img className="img" src={loop} alt="arrowsLoop" />
+          <p className="instruction">Choose things</p>
+          <p className="things__to--give">clothes, toys, equipment and more</p>
+        </div>
+      </div>
+      <div className="container__button">
+        <button
+          onClick={handleClickToDonateThing}
+          className="button__give--things"
+          type="button"
+        >
+          GIVE THINGS <br /> BACK
+        </button>
+      </div>
+    </section>
+  );
+};
